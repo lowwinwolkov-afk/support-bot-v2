fa417b4d10e5bc06d2c5c9cac1759e807fb77985
Morphix Studio <lowwinwolkov@gmail.com>
2026-05-08 12:56:47 +0600

    bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,15 @@
-from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, CallbackQueryHandler, filters
-from handlers.user_handlers import start, user_message, new_ticket
-from handlers.support_handlers import callback, support_reply
 
-TOKEN = "8288994829:AAHh5SqqJyWe_3gskGRz10sv5vLyw9ryBf0"
+from config import TOKEN
+from user_handlers import start, user_message
+from support_handlers import callback
 
-GROUP_ID = -1003979602444 # супергруппа с темами
-
-LOG_TOPIC = 44  # можно поставить id темы логов
-
-SUPPORTS = {
-    5530223549: "LowWin",
-    7123456789: "Hellsinger"
-}
 app = ApplicationBuilder().token(TOKEN).build()
 
-# Пользовательский интерфейс
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & filters.ChatType.PRIVATE, user_message))
-app.add_handler(MessageHandler(filters.TEXT & filters.ChatType.PRIVATE, new_ticket))
 
-# Чат саппорта
-app.add_handler(MessageHandler(filters.TEXT, support_reply))
+app.add_handler(MessageHandler(filters.TEXT & filters.ChatType.PRIVATE, user_message))
+
 app.add_handler(CallbackQueryHandler(callback))
 
 app.run_polling()
